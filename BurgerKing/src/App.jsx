b52fb17b273474bc4a0134c641f40a58e5e943dd
Oyeleke Toluwanimi Praise <callmimidex@gmail.com>
2024-10-22 21:59:07 -0700

    update icon color

--- BurgerKing/src/App.jsx
+++ BurgerKing/src/App.jsx
@@ -1,8 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { NavBar } from "./Components/Navbar";
+import HeroSlider from "./Components/HeroSlider";
 
-function App() {}
+function App() {
+  return (
+    <>
+      <NavBar />
+      <HeroSlider />
+    </>
+  );
+}
 
 export default App;
--- BurgerKing/src/Components/Navbar.jsx
+++ BurgerKing/src/Components/Navbar.jsx
@@ -1,13 +1,31 @@
 import logo from "../assets/Burger-King-logo.svg";
 
-export default NavBar = () => {
+export const NavBar = () => {
   return (
     <>
-      <nav>
-        <ul>
-          <div className="logo">
-            <img src={logo} alt="Burger King Logo" />
-          </div>
+      <nav className="navbar">
+        <div className="logo">
+          <img src={logo} alt="Burger King Logo" />
+        </div>
+        <ul className="navlinks-1">
+          <li>
+            <a href="#">Order</a>
+          </li>
+          <li>
+            <a href="#">Menu</a>
+          </li>
+          <li>
+            <a href="#">Offer</a>
+          </li>
+          <li>
+            <a href="#">Restaurants</a>
+          </li>
+          <li>
+            <a href="#">Careers</a>
+          </li>
+          <li>
+            <a href="#">News</a>
+          </li>
         </ul>
       </nav>
     </>
