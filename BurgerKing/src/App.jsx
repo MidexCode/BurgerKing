f5608fbb2b8eacf8d1cb05df1032a0f4644ac818
Oyeleke Toluwanimi Praise <callmimidex@gmail.com>
2024-10-23 09:12:01 -0700

    i did the locator section

--- BurgerKing/src/App.jsx
+++ BurgerKing/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { NavBar } from "./Components/Navbar";
 import HeroSlider from "./Components/HeroSlider";
+import StoreLocator from "./Components/StoreLocator";
 
 function App() {
   return (
     <>
       <NavBar />
       <HeroSlider />
+      <StoreLocator />
     </>
   );
 }
