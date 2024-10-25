a1821c29e6e378a304e4abe78b5376086314964c
Oyeleke Toluwanimi Praise <callmimidex@gmail.com>
2024-10-25 07:23:07 -0700

    Done with the footer

--- BurgerKing/src/Components/Footer.jsx
+++ BurgerKing/src/Components/Footer.jsx
@@ -11,29 +11,30 @@ export const Footer = () => {
         <div className="footer-content">
           <div className="footer-title">
             <a href="https://www.burger-king.ng/">
-              <img src={Burgerlogo} alt="The Burger Logo" />
+              <img src={Burgerlogo} alt="The Burger Logo" width={"150px"} />
             </a>
           </div>
 
           <div className="footer-row1">
-            <h5>BK® INFO</h5>
-            <p className="footer-row-link-1">
+            <h5 className="footer-row-title">BK® INFO</h5>
+
+            <p className="footer-row-link">
               <a href="https://www.burger-king.ng/about-bk/">About BK</a>
             </p>
 
-            <p className="footer-row-link-1">
+            <p className="footer-row-link">
               <a href="https://www.burger-king.ng/privacy-policy/">
                 Privacy Policy
               </a>
             </p>
 
-            <p className="footer-row-link-1">
+            <p className="footer-row-link">
               <a href="https://www.burger-king.ng/reach-out/">
                 Reach out to US
               </a>
             </p>
 
-            <p className="footer-row-link-1">
+            <p className="footer-row-link">
               <a href="https://mega.nz/file/49hBXIKQ#hWhvDUeH91ZGNkZfB9AiADeTVKUQfgNY5MfN32k9nv0">
                 Terms and Conditions
               </a>
@@ -41,17 +42,17 @@ export const Footer = () => {
           </div>
 
           <div className="footer-row2">
-            <h5>BK® Cares</h5>
+            <h5 className="footer-row-title">BK® Cares</h5>
 
-            <p className="footer-row-link-2">
+            <p className="footer-row-link">
               <a href="https://www.burger-king.ng/allergens">Allergens Info</a>
             </p>
 
-            <p className="footer-row-link-2">
+            <p className="footer-row-link">
               <a href="https://www.burger-king.ng/foodquality">Food Quality</a>
             </p>
 
-            <p className="footer-row-link-2">
+            <p className="footer-row-link">
               <a href="https://www.burger-king.ng/responsibility">
                 Responsibility
               </a>
@@ -59,13 +60,13 @@ export const Footer = () => {
           </div>
 
           <div className="footer-row3">
-            <h5>BK® and YOU</h5>
+            <h5 className="footer-row-title">BK® and YOU</h5>
 
-            <p className="footer-row-link-3">
+            <p className="footer-row-link">
               <a href="https://www.burger-king.ng/careers">Careers</a>
             </p>
 
-            <p className="footer-row-link-3">
+            <p className="footer-row-link">
               <a href="https://www.burger-king.ng/privacy-policy">
                 Privacy policy
               </a>
@@ -73,9 +74,11 @@ export const Footer = () => {
           </div>
         </div>
 
+        <hr class="footer-divider"></hr>
+
         <div className="footer-copyright-and-icons">
           <div className="footer-copyright">
-            <span>
+            <span className="footer-copyright-title">
               TM & Copyright 2021 Burger King Corporation. All Rights Reserved.
             </span>
           </div>
