a9358b32314f9ce109d16c182bdd73aed24faa5b
Oyeleke Toluwanimi Praise <callmimidex@gmail.com>
2024-10-25 18:25:33 -0700

    working on the button on the order section

--- BurgerKing/src/Components/Order/Order.jsx
+++ BurgerKing/src/Components/Order/Order.jsx
@@ -1,9 +1,61 @@
 import { Footer } from "../footer/Footer";
+import logo from "../../assets/Burger-King-logo.svg";
+import { Link } from "react-router-dom";
 
 export const Order = () => {
   return (
     <>
+      <nav className="navbar">
+        <div className="logo">
+          <Link to={"/"}>
+            <img src={logo} alt="Burger King Logo" />
+          </Link>
+        </div>
+        <ul className="navlinks-1">
+          <li>
+            <Link to="/order" className="nav-links">
+              Order
+            </Link>
+          </li>
+          <li>
+            <Link to="/menu" className="nav-links">
+              Menu
+            </Link>
+          </li>
+          <li>
+            <Link to="offers" className="nav-links">
+              Offer
+            </Link>
+          </li>
+          <li>
+            <Link to="/restaurants" className="nav-links">
+              Restaurants
+            </Link>
+          </li>
+          <li>
+            <Link to="/careers" className="nav-links">
+              Careers
+            </Link>
+          </li>
+          <li>
+            <Link to="/news" className="nav-links">
+              News
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
       <Footer />
+    </>
+  );
+};
+
+
+
+export const NavBar = () => {
+  return (
+    <>
+      
     </>
   );
 };
