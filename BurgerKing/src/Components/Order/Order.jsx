869a4c886951347dc153ce469e697831c2ac6adb
Oyeleke Toluwanimi Praise <callmimidex@gmail.com>
2024-10-25 18:38:24 -0700

    still working on the button

--- BurgerKing/src/Components/Order/Order.jsx
+++ BurgerKing/src/Components/Order/Order.jsx
@@ -1,8 +1,19 @@
+import * as React from "react";
 import { Footer } from "../footer/Footer";
 import logo from "../../assets/Burger-King-logo.svg";
 import { Link } from "react-router-dom";
+// import { Unstable_Popup as Popup } from "@mui/base";
 
 export const Order = () => {
+  const [anchor, setAnchor] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchor(anchor ? null : event.currentTarget);
+  };
+
+  const open = Boolean(anchor);
+  const id = open ? "simple-popup" : undefined;
+
   return (
     <>
       <nav className="navbar">
@@ -42,6 +53,22 @@ export const Order = () => {
               News
             </Link>
           </li>
+
+          <li>
+            <React.Fragment>
+              <button
+                type="button"
+                aria-describedby={id}
+                className="nav-button"
+                onClick={handleClick}
+              >
+                Register
+              </button>
+              {/* <BasePopup id={id} open={open} anchor={anchor}>
+                <div className="CustomPopup">The content of the Popup.</div>
+              </BasePopup> */}
+            </React.Fragment>
+          </li>
         </ul>
       </nav>
 
@@ -50,12 +77,6 @@ export const Order = () => {
   );
 };
 
-
-
 export const NavBar = () => {
-  return (
-    <>
-      
-    </>
-  );
+  return <></>;
 };
